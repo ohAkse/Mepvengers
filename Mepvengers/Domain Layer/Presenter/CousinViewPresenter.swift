989864b59a2aca85f12e4643d36a3fc79b0392d0
ohAkse <segassdc1@naver.com>
2023-08-14 21:30:00 +0900

    유튜브쪽 되는거 확인..

--- Mepvengers/Domain Layer/Presenter/CousinViewPresenter.swift
+++ Mepvengers/Domain Layer/Presenter/CousinViewPresenter.swift
@@ -48,7 +48,6 @@ class CousinViewPresenter<AnyFetchUseCase> : CousinViewPresenterSpec  where AnyF
     }
     func loadData() {
         var fetchedVideoIds = Set(googleAPI.items.compactMap { $0.id.videoId })
-        
         FetchDataUseCaseSpec.fetchDataModel(keyword, pageCount, nextToken) { [weak self] result in
             guard let self = self else { return }
             switch result {
@@ -74,40 +73,27 @@ class CousinViewPresenter<AnyFetchUseCase> : CousinViewPresenterSpec  where AnyF
         CousinViewSpec.RouteVideoPlayerController(cellInfo: cellInfo)
     }
     func OnTagSelectedItem(cellInfo : CousinViewTagModel){
-//요거 주석풀고 해보기
-//        self.keyword = cellInfo.category
-//        var fetchedVideoIds = Set(googleAPI.items.compactMap { $0.id.videoId })
-//
-//        FetchDataUseCaseSpec.fetchDataModel("도로시", pageCount, nextToken) { [weak self] result in
-//            guard let self = self else { return }
-//            switch result {
-//            case .success(let googleAPIResponse):
-//                var googleResponse = googleAPIResponse
-//                let newItems = googleResponse.items.filter { item in
-//                    guard let videoId = item.id.videoId else {
-//                        return false
-//                    }
-//                    return !fetchedVideoIds.contains(videoId)
-//                }
-//                fetchedVideoIds.formUnion(newItems.compactMap { $0.id.videoId })
-//                googleResponse.items = newItems
-//                self.googleAPI = googleResponse
-//                self.nextToken = googleResponse.nextPageToken
-//                self.CousinViewSpec.UpdateMainCollectionView(googleVideoAPI: googleResponse)
-//            case .failure(let error):
-//                self.CousinViewSpec.ShowErrorMessage(ErrorMessage: error.localizedDescription)
-//            }
-//        }
-        
-        APIManager.fetchGoogle(keyword: "매운" + cellInfo.category) { (googleResponse, networkError) in
-            if networkError == .empty {
-                var googleResponse = googleResponse
-                let filteredItem = googleResponse.items.filter { $0.id.videoId != nil }
-                googleResponse.items = filteredItem
+        self.keyword = cellInfo.category
+        var fetchedVideoIds = Set(googleAPI.items.compactMap { $0.id.videoId })
+
+        FetchDataUseCaseSpec.fetchDataModel(keyword, pageCount, nextToken) { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case .success(let googleAPIResponse):
+                var googleResponse = googleAPIResponse
+                let newItems = googleResponse.items.filter { item in
+                    guard let videoId = item.id.videoId else {
+                        return false
+                    }
+                    return !fetchedVideoIds.contains(videoId)
+                }
+                fetchedVideoIds.formUnion(newItems.compactMap { $0.id.videoId })
+                googleResponse.items = newItems
                 self.googleAPI = googleResponse
-                self.CousinViewSpec.ReloadTagCollectionView(cellInfo: self.googleAPI.items)
-            } else {
-                self.CousinViewSpec.ShowErrorMessage(ErrorMessage : networkError.localizedDescription)
+                self.nextToken = googleResponse.nextPageToken
+                self.CousinViewSpec.ReloadTagCollectionView(cellInfo: googleResponse.items)
+            case .failure(let error):
+                self.CousinViewSpec.ShowErrorMessage(ErrorMessage: error.localizedDescription)
             }
         }
     }
